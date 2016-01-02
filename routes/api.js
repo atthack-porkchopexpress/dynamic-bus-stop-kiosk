d1069bb9f1c59f4c4f1626e37c580009ad36c54c
Eric Erickson <eerickson@cbtnuggets.com>
2016-01-02 15:07:06 -0800

    More stuff

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/riderAtStop', function(req, res, next) {
+  res.json({success: true});
 });
 
 module.exports = router;
