407913c9f0897a088e37b3ed13ca0ca9767cb44a
Eric Erickson <eerickson@cbtnuggets.com>
2016-01-02 23:00:27 -0800

    Got the web app fully functional

--- public/js/bus-stop.js
+++ public/js/bus-stop.js
@@ -1,5 +1,65 @@
-function riderAtStop() {
-    $.post( "api/riderAtStop", function(data) {
-        console.log(data);
+$(document).ready(function() {
+   $('#requestBus').click(function(){
+       console.log('hey');
+       requestBusMenu();
+   });
+});
+var currentStop = '1666';
+
+function requestBus(stopId) {
+    showLoading();
+
+    $.post( 'api/riderAtStop/' + stopId, function(data) {
+        if (data.success) {
+            // Added a little delay to show the sweet bus animated gif
+            setTimeout(function() {
+                $('#bus-requested').show();
+                $('#bus-loading').hide();
+            }, 500);
+
+            setTimeout(gotoHome, 5000);
+        } else {
+            gotoHome();
+        }
     });
+}
+
+function requestBusMenu() {
+    showLoading();
+
+    $.get('api/busRoutes', function(busStops) {
+        var passedCurrentStop = false;
+        $('#stops').append( '<h3>Where would you like to go?</h3>' +
+                            '<p>These are the stops available on this bus line.</p>');
+        busStops.forEach(function(stop){
+            console.log(stop.stopId);
+            if (stop.stopId === currentStop) {
+                passedCurrentStop = true;
+            } else if (passedCurrentStop){
+                $('#stops').append( '<button type="button" ' +
+                                    'class="btn btn-primary btn-lg stop-btn" ' +
+                                    'onclick="requestBus(' + stop.stopId + ')">' +
+                                    stop.stopName + '</button>');
+                if (stop.stopId !== busStops[busStops.length-1].stopId) {
+                    $('#stops').append('<span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>');
+                }
+
+                // Added a little delay to show the sweet bus animated gif
+                setTimeout(function() {
+                    $('#bus-loading').hide();
+                    $('#stops').show();
+                }, 500);
+            }
+        });
+    });
+}
+
+function showLoading() {
+    $('.sub-page').hide();
+    $('#bus-loading').show();
+}
+
+function gotoHome() {
+    $('.sub-page').hide();
+    $('#home').show();
 }
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,34 @@
+var request = require('request');
 var express = require('express');
 var router = express.Router();
 
-router.post('/riderAtStop', function(req, res, next) {
-  res.json({success: true});
+router.get('/busRoutes', function(req, res) {
+    var getUrl = 'http://159.203.212.140:3000/v1/ericsson/route'
+    request(getUrl, function(err, getRes, body) {
+        if (!err && getRes.statusCode == 200) {
+            var busRoutes = JSON.parse(body);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(busRoutes);
+        }
+    });
+});
+
+router.post('/riderAtStop/:stopId', function(req, res) {
+    console.log('Logging rider at: ' + req.params.stopId);
+    var postUrl = 'http://159.203.212.140:3000/v1/ericsson/route/:stopId?' + req.params.stopId;
+    request.post({url: postUrl }, function(err, postRes, body){
+        var result = { success: false };
+        if (!err && postRes.statusCode == 201) {
+
+            //result = JSON.parse(body);
+            result.success = true;
+        } else {
+            console.log(postRes.statusCode);
+            console.log(err);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(result);
+    });
 });
 
 module.exports = router;
